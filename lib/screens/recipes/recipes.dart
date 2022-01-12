0782acd79acec86ff8ee2c61f521ceeb80ae4a50
Andrew Moreno <andrewmoreno1431@gmail.com>
2022-01-12 15:01:21 -0800

    renamed recipes_home to recipes

--- lib/screens/recipes/recipes.dart
+++ lib/screens/recipes/recipes.dart
@@ -1,21 +1,39 @@
+import 'package:aeroquest/constraints.dart';
+import 'package:aeroquest/screens/new_recipe/new_recipe.dart';
+import 'package:aeroquest/widgets/appbar/appbar_addButton.dart';
+import 'package:aeroquest/widgets/appbar/appbar_leading.dart';
+import 'package:aeroquest/widgets/appbar/appbar_text.dart';
 import 'package:flutter/material.dart';
 
-import '../recipes_home/local widgets/appbar.dart';
 import 'local widgets/recipe_card.dart';
 import '../../models/recipe_data.dart';
 import '../../models/recipe.dart';
+import '../../widgets/custom_drawer.dart';
 
-class RecipesHome extends StatelessWidget {
-  RecipesHome({Key? key}) : super(key: key);
+class Recipes extends StatelessWidget {
+  Recipes({Key? key}) : super(key: key);
+
+  static const routeName = "/recipes";
 
   final List<Recipe> _recipeData = RecipeData().recipes;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const CustomAppBar(),
+      appBar: AppBar(
+        backgroundColor: kBackgroundColor,
+        elevation: 0,
+        centerTitle: true,
+        leading: const AppBarLeading(function: LeadingFunction.menu),
+        title: const AppBarText(text: "RECIPES"),
+        actions: [
+          AppBarAddButton(onTap: () {
+            Navigator.pushNamed(context, NewRecipe.routeName);
+          })
+        ],
+      ),
       drawer: const Drawer(
-        child: Text("This is a drawer :)"),
+        child: CustomDrawer(),
       ),
       body: SafeArea(
         child: ListView.separated(
