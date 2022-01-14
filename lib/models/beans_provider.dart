fbafff8e3e846a75c51df9e6c7ed7b37450d5ce4
Andrew Moreno <andrewmoreno1431@gmail.com>
2022-01-14 00:12:05 -0800

    provider with dummy data for beans

---
+++ lib/models/beans_provider.dart
@@ -0,0 +1,38 @@
+import 'dart:collection';
+
+import 'package:aeroquest/models/coffee_bean_entry.dart';
+import 'package:flutter/material.dart';
+
+class BeansProvider extends ChangeNotifier {
+  final List<CoffeeBeanEntry> _beans = [
+    CoffeeBeanEntry(
+        beanName: "JJBean Bros", description: "This is a light roasted coffee"),
+    CoffeeBeanEntry(
+        beanName: "Xmas Gift from Parentals",
+        description:
+            "This is a dark roasted coffee that is yum yum in my bum bum"),
+    CoffeeBeanEntry(
+        beanName: "More coffee woohoo",
+        description: "Idk what to write here anymore"),
+    CoffeeBeanEntry(
+        beanName: "Omg theres more??",
+        description: "yes there is u lil bitch boi more coffee more coffee"),
+    CoffeeBeanEntry(beanName: "Hello", description: "There u suck"),
+  ];
+
+  UnmodifiableListView<CoffeeBeanEntry> get beans {
+    return UnmodifiableListView(_beans);
+  }
+
+  void deleteBean(int index) {
+    print("index: " + index.toString());
+    _beans.removeAt(index);
+    notifyListeners();
+  }
+
+  // void addBean(String beanName) {
+  //   _beans.add(beanName);
+  //   notifyListeners();
+  //   print(_beans);
+  // }
+}
