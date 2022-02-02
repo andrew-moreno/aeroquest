8d400972e5825f43c80fbae359cf31bc805878a0
Andrew Moreno <andrewmoreno1431@gmail.com>
2022-02-01 17:28:50 -0800

    trying to find some nice colours

--- lib/constraints.dart
+++ lib/constraints.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 
 const kBackgroundColor = Color(0xFF1C1C1E);
+const kTextColor = Color(0xFFD5C3B1);
+const kSubtitleColor = Color.fromRGBO(217, 204, 191, 0.5);
+const kCornerRadius = 10.0;
+
 const kDarkNavy = Color(0xFF191B2C);
 const kMediumNavy = Color(0xFF1F2130);
 const kLightNavy = Color(0xFF242739);
 const kAccentOrange = Color(0xFFB67C6B);
 const kAccentYellow = Color(0xFFDDB262);
-const kTextColor = Color(0xFFD5C3B1);
-const kSubtitleColor = Color.fromRGBO(217, 204, 191, 0.5);
 const kDarkSubtitleColor = Color(0xFF404665);
-const kCornerRadius = 10.0;
+
+
+// const kDarkNavy = Color(0xFF1A263D);
+// const kMediumNavy = Color(0xFF213150);
+// const kLightNavy = Color(0xFF2A3957);
+// const kAccentOrange = Color(0xFFF58C95);
+// const kAccentYellow = Color(0xFF2AB08F);
+// const kDarkSubtitleColor = Color(0xFF5875B1);
