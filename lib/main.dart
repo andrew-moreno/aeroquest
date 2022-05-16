29ade0bdba355e180efde2ce840ef7bb5d08655a
Andrew Moreno <andrewmoreno1431@gmail.com>
2022-05-15 19:27:22 -0700

    adding new material animations and provider functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:aeroquest/models/recipes_provider.dart';
+import 'package:control_style/decorated_input_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -5,6 +7,7 @@ import 'package:aeroquest/screens/about_aeroquest/about_aeroquest.dart';
 import 'package:aeroquest/screens/coffee_beans/coffee_beans.dart';
 import 'package:aeroquest/constraints.dart';
 import 'package:aeroquest/screens/recipes/recipes.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,67 +19,70 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(392.73, 759.27),
-      builder: () => MaterialApp(
-        theme: ThemeData(
-          backgroundColor: kPrimary,
-          canvasColor: kPrimary,
-          textTheme: const TextTheme(
-            headline1: TextStyle(
-              color: kLightSecondary,
-              fontSize: 32,
-              fontWeight: FontWeight.bold,
-              fontFamily: "Spectral",
-            ),
-            headline2: TextStyle(
-              color: kLightSecondary,
-              fontSize: 27,
-              fontWeight: FontWeight.bold,
-              fontFamily: "Spectral",
-            ),
-            headline3: TextStyle(
-              color: kLightSecondary,
-              fontFamily: "Spectral",
-              fontWeight: FontWeight.w700,
-              fontSize: 24,
-            ),
-            headline4: TextStyle(
-              color: kPrimary,
-              fontFamily: "Poppins",
-              fontWeight: FontWeight.w500,
-              fontSize: 25,
-            ),
-            subtitle1: TextStyle(
-              fontFamily: "Poppins",
-              fontWeight: FontWeight.w500,
-              fontSize: 13,
+    return ChangeNotifierProvider(
+      create: (_) => RecipesProvider(),
+      builder: (context, child) {
+        return MaterialApp(
+          theme: ThemeData(
+            useMaterial3: true,
+            backgroundColor: kPrimary,
+            canvasColor: kPrimary,
+            textTheme: const TextTheme(
+              headline1: TextStyle(
+                color: kLightSecondary,
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                fontFamily: "Spectral",
+              ),
+              headline2: TextStyle(
+                color: kLightSecondary,
+                fontSize: 27,
+                fontWeight: FontWeight.bold,
+                fontFamily: "Spectral",
+              ),
+              headline3: TextStyle(
+                color: kLightSecondary,
+                fontFamily: "Spectral",
+                fontWeight: FontWeight.w700,
+                fontSize: 24,
+              ),
+              headline4: TextStyle(
+                color: kPrimary,
+                fontFamily: "Poppins",
+                fontWeight: FontWeight.w500,
+                fontSize: 25,
+              ),
+              subtitle1: TextStyle(
+                fontFamily: "Poppins",
+                fontWeight: FontWeight.w500,
+                fontSize: 13,
+              ),
             ),
           ),
-        ),
-        initialRoute: Recipes.routeName,
-        onGenerateRoute: (settings) {
-          if (settings.name == Recipes.routeName) {
-            return PageRouteBuilder(
-                settings:
-                    settings, // Pass this to make popUntil(), pushNamedAndRemoveUntil(), works
-                pageBuilder: (_, __, ___) => Recipes(),
-                transitionsBuilder: (_, a, __, c) =>
-                    FadeTransition(opacity: a, child: c));
-          }
-          if (settings.name == CoffeeBeans.routeName) {
-            return PageRouteBuilder(
-                settings:
-                    settings, // Pass this to make popUntil(), pushNamedAndRemoveUntil(), works
-                pageBuilder: (_, __, ___) => const CoffeeBeans(),
-                transitionsBuilder: (_, animation, __, child) =>
-                    FadeTransition(opacity: animation, child: child));
-          }
-        },
-        routes: {
-          AboutAeroquest.routeName: (ctx) => const AboutAeroquest(),
-        },
-      ),
+          initialRoute: Recipes.routeName,
+          onGenerateRoute: (settings) {
+            if (settings.name == Recipes.routeName) {
+              return PageRouteBuilder(
+                  settings:
+                      settings, // Pass this to make popUntil(), pushNamedAndRemoveUntil(), works
+                  pageBuilder: (_, __, ___) => Recipes(),
+                  transitionsBuilder: (_, a, __, c) =>
+                      FadeTransition(opacity: a, child: c));
+            }
+            if (settings.name == CoffeeBeans.routeName) {
+              return PageRouteBuilder(
+                  settings:
+                      settings, // Pass this to make popUntil(), pushNamedAndRemoveUntil(), works
+                  pageBuilder: (_, __, ___) => const CoffeeBeans(),
+                  transitionsBuilder: (_, animation, __, child) =>
+                      FadeTransition(opacity: animation, child: child));
+            }
+          },
+          routes: {
+            AboutAeroquest.routeName: (ctx) => const AboutAeroquest(),
+          },
+        );
+      },
     );
   }
 }
